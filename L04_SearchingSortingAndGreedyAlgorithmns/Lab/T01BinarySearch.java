e67bfb8f06b6b5c4e24395abc7e4dc100ee80173
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-04 18:46:18 +0200

    Add files via upload

---
+++ L04_SearchingSortingAndGreedyAlgorithmns/Lab/T01BinarySearch.java
@@ -0,0 +1,36 @@
+package M04_AlgorithmsFundamentals.L04_SearchingSortingAndGreedyAlgorithmns.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T01BinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int key = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(indexOf(arr, key));
+    }
+
+    private static int indexOf(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int curr = arr[mid];
+            if (key < curr) {
+                end = mid - 1;
+            } else if (key > curr) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
